50d28953f797dc0bacc1fd6e9630c5649c074e1c
Ubuntu <ubuntu@ip-172-31-18-31.eu-west-1.compute.internal>
2017-01-15 00:47:21 +0000

    modified hyperband

--- assignment2/q2_hyperband.py
+++ assignment2/q2_hyperband.py
@@ -1,14 +1,16 @@
 import numpy as np
 from q2_NER import get_hyperparams_config, hyperband_train_and_validate
 
-max_iter = 27 # maximum iterations/epochs per configuration
-eta = 3 # defines downsampling rate (default=2)
+max_iter = 2 # maximum iterations/epochs per configuration
+eta = 2 # defines downsampling rate (default=2)
 logeta = lambda x: np.log(x)/np.log(eta)
 s_max = int(logeta(max_iter))  # number of unique executions of Successive Halving (minus one)
 B = (s_max+1)*max_iter  # total number of iterations (without reuse) per execution of Succesive Halving (n,r)
 
 #### Begin Finite Horizon Hyperband outlerloop. Repeat indefinetely.
 losses_dict = dict()
+best_config = None
+best_val_loss = float('inf')
 for s in reversed(range(s_max+1)):
 	n = int(np.ceil(B/max_iter/(s+1)*eta**s)) # initial number of configurations
 	r = max_iter*eta**(-s) # initial number of iterations to run configurations for
@@ -22,8 +24,17 @@ for s in reversed(range(s_max+1)):
                 print('number of configurations: ' + str(n_i) + ', epochs to run: ' + str(r_i))
 		val_losses = [ hyperband_train_and_validate(r_i, t) for t in T ]
 
+                indices = np.argsort(val_losses)
+                idx = indices[0]
+                curr_val_loss = val_losses[idx]
+                curr_best_config = T[idx]
+
 		losses_dict = dict(zip(T, val_losses))
-		T = [ T[i] for i in np.argsort(val_losses)[0:int( n_i/eta )] ]
+		T = [ T[i] for i in indices[0:int( n_i/eta )] ]
+
+        if(curr_val_loss < best_val_loss):
+                best_val_loss = curr_val_loss
+                best_config = curr_best_config
 	#### End Finite Horizon Successive Halving with (n,r)
 
 target = open('losses_dict', 'w')
